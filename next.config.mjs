313ed636f5e81b7dd3ff379c79db3d4d067b9ac5
olearykieran <koleary515@gmail.com>
2024-12-04 14:06:29 -0500

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
+const isProd = process.env.NODE_ENV === "production";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export", // Enable static export
+  assetPrefix: isProd ? "/sauna-master/" : "",
+  trailingSlash: true, // Ensure URLs have trailing slashes for GitHub Pages
+};
 
 export default nextConfig;
